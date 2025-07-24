57907a2a154c55a7c77956e510250e15419d35af
Devashish Roy <medevashishroy@gmail.com>
2025-07-24 15:33:01 +0530

    fix: improve UX of NavBar.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import AuthProvider from "@/context/AuthProvider";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/custom/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
